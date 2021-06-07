88da4456164ccc405bdf3151ff015add9af5bbeb
mario-korban <mario.korban@spexal.com>
2021-06-07 23:50:42 +0300

    Add Local Files

---
+++ app.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+from flask import Flask, request, jsonify, render_template
+import json
+from methods import *
+
+app = Flask(__name__)
+# app.run(debug=True)
+
+arrayOfDenoised = []
+arrayofTags = []
+arrayofTagValues = []
+
+@app.route('/')
+def index():
+    texts = parseText()
+    return render_template('index.html', phrases=texts, filtered = False)
+
+@app.route('/filteredRes')
+def filtered_index():
+    filtered = parseFilteredText()
+    return render_template('index.html', phrases=filtered, filtered = True)
+
+def parseText():
+    testsite_array = []
+    with open('phrases.txt') as my_file:
+        testsite_array = my_file.readlines()
+    
+    return testsite_array
+
+def parseFilteredText():
+    testsite_array = []
+    filtered_testsite_array = []
+
+    with open('phrases.txt') as my_file:
+        testsite_array = my_file.readlines()
+    
+    with open('data.json', 'r') as fq:
+        try:
+            dataaz = json.load(fq)
+            for d in dataaz:
+                segments = d["segments"]
+                for seg in segments:
+                    for s in seg:
+                        if(s["flag"] == "flag"):
+                            filtered_testsite_array.append(d["original"])
+                            continue
+                        else:
+                            continue
+        except:
+            dataaz = []
+
+    # print(filtered_testsite_array)
+    # for tex in testsite_array:
+    return list(dict.fromkeys(filtered_testsite_array))
+
+@app.route('/getdata/<toSend>', methods=['GET','POST'])
+def data_get(toSend):
+    
+    if request.method == 'POST': # POST request
+        print(request.get_text())  # parse as text
+        return 'OK', 200
+    
+    else: # GET request
+        # print(toSend)
+        
+        with open('data.json', 'r') as fq:
+            try:
+                dataaz = json.load(fq)
+                newData = json.loads(toSend)
+                for d in dataaz:
+                    if(d["original"] == newData["original"]):
+                        # ind = dataaz.index(d)
+                        newData["raw"] = d["raw"]
+                        dataaz.remove(d)
+                        # dataaz.append(newData)
+                        break
+                        # print("The Same We need to Delete")
+                dataaz.append(newData)
+                with open('data.json', 'w') as f:
+                    json.dump(dataaz, f, ensure_ascii = False)
+                    return "Success"
+            except:
+                dataaz = []
+
+@app.route('/getAnnotationStatus/<phrase>', methods=['GET','POST'])
+def annotation_get(phrase):
+    
+    if request.method == 'POST': # POST request
+        print(request.get_text())  # parse as text
+        return 'OK', 200
+    
+    else: # GET request
+        # print(toSend)
+        print(phrase)
+        if(checkIfAnnotated(phrase)):
+            return "annotated"
+        else:
+            return "notAnnotated"
+
+        # return "Success"
+
+@app.route('/getPreviousAnnotations/<phrase>', methods=['GET','POST'])
+def previous_annotation_get(phrase):
+    
+    if request.method == 'POST': # POST request
+        print(request.get_text())  # parse as text
+        return 'OK', 200
+    
+    else: # GET request
+        print(phrase)
+        with open('data.json', 'r') as fq:
+            try:
+                dataaz = json.load(fq)
+                # find specific phrase to load it's params
+                for d in dataaz:
+                    if(d["original"]==phrase):
+                        return json.dumps(d)
+            except:
+                dataaz = []
+
+        # return "Success"
+
+def checkIfAnnotated(phrase):
+    phrasesAnnotated = []
+    with open('data.json', 'r') as fq:
+        try:
+            dataaz = json.load(fq)
+            print(dataaz)
+            for d in dataaz:
+                phrasesAnnotated.append((d["original"]))
+
+            if (phrase in phrasesAnnotated):
+                return True
+            else:
+                return False
+        except:
+            dataaz = {}            
+
+@app.route('/getSearch/<data>', methods=['GET','POST'])
+def get_search(data):
+    if request.method == 'POST': # POST request
+        print(request.get_text())  # parse as text
+        return 'OK', 200
+    else: # GET request
+        new_data = json.loads(data)
+        print(new_data)
+        json_response = search_bar_examples(new_data["search_txt0"], examples_json, (new_data["search_txt1"], new_data["search_txt2"]))
+        return json_response
+
+    print(json_response)
+
+with open('./pos_examples.json') as f:
+    examples_json = json.load(f)
+app.run(host='0.0.0.0')
---
+++ methods.py
@@ -0,0 +1,229 @@
+from typing import Dict, List, Union, Tuple
+from string import punctuation as punc
+from collections import namedtuple
+import re
+import os
+import json
+
+from git import Repo, RemoteReference
+
+POS = str
+ExampleField = str # ['baseword', 'gloss', 'clitic']
+SegmentType = str # ['baseword', 'enclitic', 'proclitic']
+Example = Dict[ExampleField, str]
+ExamplesQueryFilter = namedtuple('ExamplesQueryFilter', 'segment_type match')
+
+ARABIC_LETTERS = "ءؤئابتثجحخدذرزسشصضطظعغفقكلمنهوىي"
+
+def search_bar_examples(query: str,
+                        examples_json: Dict[SegmentType, Dict[POS, List[Example]]],
+                        query_filter: Tuple[str] = ('Baseword', 'Approximate')
+                        ) -> Dict[POS, List[Example]]:
+    """Function which allows to search for specific examples in a static JSON file using a filter.
+    For the query filter:
+        - In index 0, possible choices are 'Baseword', 'Enclitic', 'Proclitic'.
+        - In index 1, possible choices are 'Approximate', 'Exact' (whether the match should be exact or not).
+
+    Args:
+        query (str): what the user will type in the search bar.
+        examples_json (Dict[SegmentType, Dict[POS, List[Dict[ExampleField, str]]]]): already parsed static JSON object sitting in memory
+        query_filter (Optional[List[str]], optional): Should be drop down menus where the user specifies what he is searching for. Defaults to ['Baseword', 'Approximate'].
+
+    Returns:
+        Dict[POS, List[Example]]: Dictionary with POS tags as keys and a list of examples to display as values.
+    """
+    query_filter = ExamplesQueryFilter(*query_filter)
+
+    is_pos = True if query.translate(
+        str.maketrans('', '', punc)).isupper() else False
+    is_arabic_str = True if query[0] in ARABIC_LETTERS else False
+    is_gloss = True if [True for char in query if char.islower(
+    ) and char not in ARABIC_LETTERS] else False
+
+    examples_json: Dict[POS, List[Example]] = examples_json[query_filter.segment_type.lower()]
+
+    response: Dict[POS, List[Example]] = {}
+    if is_pos:
+        for k, v in examples_json.items():
+            if query_filter.match.lower() == 'approximate':
+                if query in k:
+                    response[k] = v
+            elif query_filter.match.lower() == 'exact':
+                if query == k:
+                    response[k] = v
+
+    elif is_arabic_str or is_gloss:
+        example_key = 'baseword' if query_filter.segment_type.lower() == 'baseword' else 'clitic'
+        example_key = 'gloss' if is_gloss else example_key
+        for k, v in examples_json.items():
+            v_: List[Example] = []
+            for example in v:
+                if query_filter.match.lower() == 'approximate':
+                    if query in example[example_key]:
+                        v_.append(example)
+                elif query_filter.match.lower() == 'exact':
+                    if query == example[example_key]:
+                        v_.append(example)
+            response.setdefault(k, []).append(v_)
+    
+    return response
+
+Annotator = str
+Feature = str # ['raw', 'coda', 'segments']
+AnnotationField = str  # ['text', 'verb_form', 'pos', 'lemma']
+Segment = Dict[AnnotationField, str]
+Annotation = Dict[Feature, Union[str, List[str], List[Segment]]]
+FilteredAnnotation = namedtuple('FilteredAnnotation', 'annotator id annotation')
+AnnotationsQueryFilter = namedtuple('AnnotationsQueryFilter', 'feature field match annotators')
+
+# Probably not the final list
+ANNOTATORS = ['Christian', 'Jana', 'Wiaam', 'Sarah', 'Carine']
+
+def search_bar_previous_annotations(query: str,
+                                    annotations_json: Dict[Annotator, List[Annotation]],
+                                    query_filter: Tuple[str] = ('Raw', 'Text', 'Approximate', 'Christian')
+                                    ) -> List[Annotation]:
+    """Function which allows to search for previously done annotations the contents of which match the values of the filter.
+    For the query filter:
+        - In index 0, possible choices are 'Raw', 'CODA', 'Segments'.
+        - In index 1, possible choices are 'Text', 'Verb Form', 'POS', 'Lemma'.
+        - In index 2, possible choices are 'Approximate', 'Exact' (whether the match should be exact or not).
+        - In index 3, possible choices are the names of the annotators, to be defined in a separate list.
+
+    Args:
+        query (str): what the user will type in the search bar.
+        annotations_json (Dict[Annotator, List[Annotation]]): JSON file containing the already finished annotations
+        query_filter (Tuple[str], optional): Should be dropdown menus where the user specifies what he is searching for. Defaults to ('Raw', 'Text', 'Approximate', 'Christian').
+
+    Returns:
+        List[Annotation]: List of annotations to show that we can scroll through.
+    """
+    query_filter = AnnotationsQueryFilter(*query_filter)
+
+    # Filtering by annotator
+    annotators = ANNOTATORS
+    annotators = [query_filter.annotators] if query_filter.annotators in ANNOTATORS else ANNOTATORS
+    if query_filter.annotators in [f'All But {annotator}' for annotator in ANNOTATORS]:
+        annotators.remove(re.sub('All But ', '', query_filter.annotators))
+    
+    # Filtering by feature
+    annotations_filtered: List[FilteredAnnotation] = []
+    for annotator, annotations in annotations_json.items():
+        if annotator in annotators:
+            for i, annotation in enumerate(annotations):
+                if query_filter.feature.lower() == 'segments':
+                    for token in annotation[query_filter.feature.lower()]:
+                        for segment in token:
+                            annotations_filtered.append(FilteredAnnotation(
+                                annotator, i, segment[query_filter.field.lower()]))
+                else:
+                    annotations_filtered.append(FilteredAnnotation(
+                        annotator, i, annotation['_'.join(query_filter.feature.lower().split())]))
+
+    already_added = []
+    response: List[Annotation] = []
+    for annotation in annotations_filtered:
+        if query_filter.match.lower() == 'approximate':
+            if query in annotation.annotation and \
+                (annotation.annotator, annotation.id) not in already_added:
+                response.append(
+                    annotations_json[annotation.annotator][annotation.id])
+        elif query_filter.match.lower() == 'exact':
+            if query == annotation.annotation and \
+                (annotation.annotator, annotation.id) not in already_added:
+                response.append(
+                    annotations_json[annotation.annotator][annotation.id])
+
+    return response
+
+
+COMMIT_MESSAGE = 'No message'
+
+def clone_repo(repo_dir='/Users/spexal/Documents/annotation_software/annotation_repo',
+               username='christios',
+               password='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',
+               repo_name='lebanese-arabic-corpus',
+               annotator_name='Wiaam') -> None:
+    """This method is called once, when the annotator sets up their local application.
+    What it does:
+        - Clones a remote repository that I have already set up
+        - Sets up a local branch in the annotator's name and its corresponding up-stream branch
+    """
+    repo_url = f"https://{username}:{password}@github.com/{username}/{repo_name}.git"
+    repo = Repo.clone_from(repo_url, repo_dir)
+    origin = repo.remote('origin')
+    current = repo.create_head(annotator_name)
+    current.checkout()
+    origin.push(annotator_name)
+    # Create up-stream branch
+    repo.head.reference = repo.create_head(annotator_name)
+    rem_ref = RemoteReference(repo, f"refs/remotes/origin/{annotator_name}")
+    repo.head.reference.set_tracking_branch(rem_ref)
+
+
+def sync_annotations(repo_dir='/Users/chriscay/thesis/annotation',
+                     annotator_name='Christian') -> None:
+    """This method is called each time the annotator presses the `Sync` button.
+    What it does:
+        - Checks out the branch in the annotator's name
+        - Commits the contents of the working directory
+        - Pushes the commit to the remote branch in the annotator's name
+        - Checks out local main
+        - Locally merges all the remote branches (one for each annotator) into local main
+        - Checks out the branch in the annotator's name again
+    """
+    repo = Repo(repo_dir)
+    repo.git.checkout(annotator_name)
+    annotator_file_path = f'{annotator_name}.json'
+    open(os.path.join(repo_dir, annotator_file_path), 'w').close()
+    repo.index.add([annotator_file_path])
+    repo.index.commit(COMMIT_MESSAGE)
+    repo.git.push('origin', annotator_name)
+
+    repo.git.fetch()
+    remote_branches = repo.git.branch('-r').splitlines()
+    remote_branches = [branch.strip()
+                       for branch in remote_branches if re.match(r'\w+/\w+$', branch.strip(), re.M) and 'main' not in branch]
+    repo.git.checkout('main')
+    for branch in remote_branches:
+        repo.git.merge(branch.strip())
+    repo.git.checkout(annotator_name)
+
+
+def get_merged_json(repo_dir='/Users/chriscay/thesis/annotation',
+                    annotator_name='Christian') -> Dict[Annotator, List[Annotation]]:
+    """This method should be called to get the JSON file with the annotator's respective
+    annotations. This is the file which should be edited by the platform in the working 
+    directory.
+    """
+    repo = Repo(repo_dir)
+    repo.git.checkout('main')
+    annotator_file_paths = [file_path for file_path in os.listdir(repo_dir) if '.json' in file_path]
+    annotations_json: Dict[Annotator, List[Annotation]] = {}
+    for annotator_file_path in annotator_file_paths:
+        with open(os.path.join(repo_dir, annotator_file_path)) as f:
+            try:
+                annotations_json[annotator_file_path.strip(
+                    '.json')] = json.load(f)
+            except json.JSONDecodeError:
+                annotations_json[annotator_file_path.strip(
+                    '.json')] = []
+    repo.git.checkout(annotator_name)
+    return annotations_json
+
+
+with open('./pos_examples.json') as f:
+    examples_json = json.load(f)
+
+# with open('/Users/chriscay/Library/Containers/com.apple.mail/Data/Library/Mail Downloads/6A3F79B7-E791-498F-87DD-A0238023A21E/data.json') as f:
+#     annotations_json = json.load(f)
+
+# search_bar_previous_annotations('p1', annotations_json, ('Segments', 'POS', 'Approximate', 'Christian'))
+search_bar_examples('PREP', examples_json, ('Enclitic', 'Approximate'))
+
+
+# clone_repo()
+# sync_annotations(repo_dir='/Users/chriscay/thesis/annotation_wiaam',
+#                     annotator_name='Wiaam')
+# get_merged_json(repo_dir='/Users/chriscay/thesis/annotation_wiaam',
+#                 annotator_name='Wiaam')
